2c46ef6a85936271cb62dad7ce13a5043097bdf8
Patrycja <p.kubiczek@gmail.com>
2022-03-26 00:00:26 +0100

    update race item

--- src/components/RaceItem.tsx
+++ src/components/RaceItem.tsx
@@ -2,10 +2,8 @@ import './RaceItem.scss'
 
 const RaceItem = (props: { active: boolean; id: number; name: string; }) => {
     return (
-        <li
-            className={`race__item ${props.active ? 'active' : ''}`} 
-        >
-            <p style={{ paddingRight: "10px" }} key={Math.random()}>
+        <li className={`race__item ${props.active ? "active" : ""}`}>
+            <p className="race__item__number" key={Math.random()}>
                 {props.id}
             </p>
             <h3 key={Math.random()}>{props.name}</h3>
